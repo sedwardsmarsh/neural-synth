9a1d0b5b13ae239f7b903fb7ce10902de686da61
Samuel Edwards-Marsh <sam.david.marsh@gmail.com>
2022-05-17 13:10:27 -0700

    expanded the columns appended to the returned dataframe. dataframes should be created higher upstream in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -8,6 +8,7 @@ import glob
 import pandas as pd
 from datetime import datetime
 from sklearn import preprocessing
+import pickle
 
 
 SIMPLE_DATASET_PATH = './data/simple_dataset'
@@ -23,6 +24,7 @@ def normalize(array) -> list:
     return normalized_array.tolist(), weight
 
 
+# TODO return a dataframe
 def extract_features(file_name: str) -> list[int]:
     '''Extracts features from the file.
     '''
@@ -60,18 +62,44 @@ def make_df(data_path: str=SIMPLE_DATASET_PATH) -> pd.DataFrame:
     # Iterate through each sound file and extract the features
     file_paths = glob.glob(f'{data_path}/*')
     for i, file_name in enumerate(file_paths):
-        if file_name[-3:] == 'csv':
+        # TODO restructure dataset directory heirarchy so this check isn't necessary
+        if file_name[-3:] in ['csv', 'pkl']:
             continue
         label = extract_labels(file_name)
         data = extract_features(file_name)
-        label, label_weight = normalize(label)
-        data, _ = normalize(data)
-        features.append([file_name, data, label, label_weight])
+        # label, label_weight = normalize(label)
+        # data, _ = normalize(data)
+        example = []
+        example.append(file_name)
+        example.extend(data)
+        example.extend(label)
+        features.append(example)
+        # features.append(label_weight)
         if i > 0 and i % (len(file_paths) * .1) == 0:
             print(f'{i/len(file_paths)}% complete')
     print('done')
-    # Convert into a Pandas dataframe 
-    return pd.DataFrame(features, columns=['file name','features','labels','label norm weight'])
+    # Convert into a Pandas dataframe
+    # TODO programmatically generate this list 
+    df = pd.DataFrame(features, columns=[
+        'file name',
+        'mfcc1','mfcc2','mfcc3','mfcc4','mfcc5','mfcc6','mfcc7','mfcc8','mfcc9',
+        'mfcc10','mfcc11','mfcc12','mfcc13','mfcc14','mfcc15','mfcc16',
+        'rms1','rms2','rms3',
+        'zcr1','zcr2','zcr3',
+        'sc1','sc2','sc3','sc4','sc5','sc6','sc7','sc8','sc9','sc10','sc11',
+        'sc12','sc13','sc14','sc15','sc16','sc17','sc18','sc19','sc20','sc21',
+        'label1','label2','label3'])
+    # normalize all the data columns. L2 normalization
+    df.loc[:, 'mfcc1':'sc21']
+    feature_weights = np.sqrt(np.sum(np.power(df.loc[:, 'mfcc1':'sc21'],2)))
+    df.loc[:, 'mfcc1':'sc21'] = df.loc[:, 'mfcc1':'sc21']/feature_weights
+    # noralize all label columns and save their de-normalization weight. L2 Normalization
+    df.loc[:, 'label1':'label3']
+    label_weights = np.sqrt(np.sum(np.power(df.loc[:, 'label1':'label3'],2)))
+    df.loc[:, 'label1':'label3'] = df.loc[:, 'label1':'label3']/label_weights
+    with open(f'{SIMPLE_DATASET_PATH}/label_denorm_weights.pkl', 'wb') as f:
+        pickle.dump(list(label_weights), f)
+    return df
 
 
 # extract features and build dataframe
